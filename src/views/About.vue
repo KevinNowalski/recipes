d90135d301fe26a9c07f9d4232a9c00d3e9b0da2
Kevin Nowalski <kevinnowalskisite@gmail.com>
2021-09-30 23:57:07 -0400

    Images added and mark-up update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="about">
-    <h1>About Recipes App</h1>
+  <div class="about"><br>
+    <h1>About Recipes App</h1><br>
     <p>The Recipes App pulls data from the API.  It creates a list view which includes all the recipes.  It also creates a recipe detail view to display each recipe.  The Ingredients with a matching ingredientId listed in the specials response should also show the special title, type and text under the ingredient name</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <h1>Recipes App</h1>
+  <div class="home"><br>
+    <h1>Recipes Home</h1><br>
     <p>The Recipes App pulls data from the API.  It creates a list view which includes all the recipes.  It also creates a recipe detail view to display each recipe.  The Ingredients with a matching ingredientId listed in the specials response should also show the special title, type and text under the ingredient name</p><br>
     <router-link to="/recipes" class="recipe"><h2>Go to recipes</h2></router-link>
   </div>
